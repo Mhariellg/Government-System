005ddc447ba25e51ad5177458ad94871de60e7e8
Mhariellg <116284702+Mhariellg@users.noreply.github.com>
2023-01-17 00:37:58 +0800

    Add files via upload

---
+++ BufferedWritter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package governmentsystem;
+
+import java.io.FileWriter;
+
+/**
+ *
+ * @author gesto
+ */
+public class BufferedWritter {
+
+    public BufferedWritter(FileWriter fw) {
+    }
+    
+}
